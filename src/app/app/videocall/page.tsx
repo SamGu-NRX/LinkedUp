3d6afa50ab9a20282d61b2e8070eef3003ad81e9
AndrewWang <andrewwang123118@gmail.com>
2025-02-23 15:57:59 -0600

    added video meeting

---
+++ src/app/app/videocall/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from "react"
+import VideoMeeting from "@/components/videocall/VideoMeeting"
+
+export default function VideoCallPage() {
+  return (
+    <VideoMeeting />
+  )
+}
+
