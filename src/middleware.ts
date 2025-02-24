0b836d9f00c2ce6b78b6a6a0bc37dd45e22e96bf
SamGu-NRX <sgu07966@gmail.com>
2025-02-24 16:53:45 -0600

    chore: update packages, include .env.local, fix lander login issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const protectedRoute = createRouteMatcher([
-  "/",
-  "/upcoming",
-  "/meeting(.*)",
-  "/previous",
-  "/recordings",
-  "/personal-room",
-]);
+const protectedRoute = createRouteMatcher(["/app/(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-
   if (protectedRoute(req)) await auth.protect();
 });
 
